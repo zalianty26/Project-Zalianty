e59d74856873623ed5b4d78c37e0eb85cfa765ea
zalianty26 <zaliantyb@gmail.com>
2022-07-26 11:55:48 +0700

    soalproject

---
+++ soal2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        pilih <input type ="text" name="pilih">
+        <br>
+        <input type ="submit" name="submit">
+</body>
+</html>
